a6e8d05dc5589e13ae3adc860b5b2b6217924edb
Yuri Glushenkov <1arxemond1@gmail.com>
2018-01-28 22:54:39 +0300

    description jmh benchmarking has been changed a little

--- src/jmh/asm/JustPlayingWithAsmProfiler.java
+++ src/jmh/asm/JustPlayingWithAsmProfiler.java
@@ -6,30 +6,38 @@ import org.openjdk.jmh.annotations.*;
 
 /**
  * https://habrahabr.ru/post/347124/ + https://github.com/dtrace4linux/linux#introduction + https://eax.me/dtrace/
+ *
+ * https://shipilev.net/talks/jvmls-July2014-benchmarking.pdf + https://github.com/AdoptOpenJDK/jitwatch/issues/84 + http://psy-lob-saw.blogspot.ru/2015/07/jmh-perfasm.html
  * 1) download (make) hsdis-amd64.so
  * 2) move hsdis-amd64.so in $JAVA_HOME/jre//lib/amd64/server/
  *
  * )VMOptions -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly
  *
  * run java with hsdis-amd64.so in $JAVA_HOME/jre//lib/amd64/server/hsdis-amd64.so
- /usr/lib/jvm/jdk-8u151-linux-x64/jdk1.8.0_151/bin/java -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly \
+ /usr/lib/jvm/jdk-8u151-linux-x64/jdk1.8.0_151/bin/java -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:PrintAssemblyOptions=syntax \
 -cp /home/glushenkov/.m2/repository/org/openjdk/jmh/jmh-core/1.20/jmh-core-1.20.jar:\
 /home/glushenkov/.m2/repository/net/sf/jopt-simple/jopt-simple/4.6/jopt-simple-4.6.jar:\
 /home/glushenkov/.m2/repository/org/apache/commons/commons-math3/3.2/commons-math3-3.2.jar:\
 ./target/classes/ \
  jmh.asm.JustPlayingWithAsmProfiler
+
+ /usr/lib/jvm/jdk-8u151-linux-x64/jdk1.8.0_151/bin/java -XX:+UnlockDiagnosticVMOptions \
+ -cp /home/glushenkov/.m2/repository/org/openjdk/jmh/jmh-core/1.20/jmh-core-1.20.jar:\
+/home/glushenkov/.m2/repository/net/sf/jopt-simple/jopt-simple/4.6/jopt-simple-4.6.jar:\
+/home/glushenkov/.m2/repository/org/apache/commons/commons-math3/3.2/commons-math3-3.2.jar:\
+./target/classes/ \
+ jmh.asm.JustPlayingWithAsmProfiler -prof perfasm
  */
 
 @State(Scope.Benchmark)
 public class JustPlayingWithAsmProfiler {
     public static void main(String[] args) throws Exception {
-        System.exit(1);
         org.openjdk.jmh.Main.main(args);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    @Fork(value = 20, warmups = 1)
+    @Fork(value = 1, warmups = 1)
     @Warmup(iterations = 1)
     @Measurement(iterations = 1)
     public double log(BenchmarkState x) {
